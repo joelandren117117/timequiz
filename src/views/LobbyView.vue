772d76687f68ac13240ce431be075d327d839117
lowenhielme <lowenhielme@gmail.com>
2025-12-03 15:48:29 +0100

    Test

--- src/views/LobbyView.vue
+++ src/views/LobbyView.vue
@@ -1,47 +1,45 @@
 <template>
   <div>
     {{pollId}}
-    <div v-if="!joined">
-      <input type="text" v-model="userName">
-      <button v-on:click="participateInPoll">
-        {{ this.uiLabels.participateInPoll }}
-      </button>
-    </div>
-    <div v-if="joined">
-      <p>Waiting for host to start poll</p>
-      {{ participants }}
-  </div>
+    <QuestionComponent v-bind:question="question"
+              v-on:answer="submitAnswer($event)"/>
+    <hr>
+    <span>{{submittedAnswers}}</span>
   </div>
 </template>
 
 <script>
+// @ is an alias to /src
+import QuestionComponent from '@/components/QuestionComponent.vue';
 import io from 'socket.io-client';
 const socket = io("localhost:3000");
 
 export default {
-  name: 'LobbyView',
+  name: 'PollView',
+  components: {
+    QuestionComponent
+  },
   data: function () {
     return {
-      userName: "",
+      question: {
+        q: "",
+        a: []
+      },
       pollId: "inactive poll",
-      uiLabels: {},
-      joined: false,
-      lang: localStorage.getItem("lang") || "en",
-      participants: []
+      submittedAnswers: {}
     }
   },
   created: function () {
     this.pollId = this.$route.params.id;
+    socket.on( "questionUpdate", q => this.question = q );
+    socket.on( "submittedAnswersUpdate", answers => this.submittedAnswers = answers );
     socket.on( "uiLabels", labels => this.uiLabels = labels );
-    socket.on( "participantsUpdate", p => this.participants = p );
-    socket.on( "startPoll", () => this.$router.push("/poll/" + this.pollId) );
-    socket.emit( "joinPoll", this.pollId );
     socket.emit( "getUILabels", this.lang );
+    socket.emit( "joinPoll", this.pollId );
   },
   methods: {
-    participateInPoll: function () {
-      socket.emit( "participateInPoll", {pollId: this.pollId, name: this.userName} )
-      this.joined = true;
+    submitAnswer: function (answer) {
+      socket.emit("submitAnswer", {pollId: this.pollId, answer: answer})
     }
   }
 }
