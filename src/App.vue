7fa559f2109e150b7b34d14c28682bc214713f40
Jonna Lundell <jonnalundell@Macbook-Air-Jonna.local>
2026-01-09 09:50:35 +0100

    initialiserat UI labels i appmontering

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import {onMounted } from 'vue';
+import { initUILabelListener, setLanguage, uiState } from './stores/uiStore';
+
+onMounted(()=>{
+  initUILabelListener();
+  setLanguage(uiState.lang);
+});
 </script>
 
 <template>
